bf1a2e192e7db818962d8fc7c5696c7f4e522f69
dengerbhai00820-com <dengerbhai00820@gmail.com>
2025-12-06 08:42:09 -0800

    add files

---
+++ index.js
@@ -0,0 +1,26 @@
+//ECMA SCRIPT EX.1 TO EX.10  
+
+// Question N.01 ALERT  
+
+// .1 alert("Hello world")
+
+// .2 alert("ABDUREHMAN")
+
+// .3  alert("123")
+
+// .4 var name = "ABDUREHMAN"
+//    alert(name)
+
+// .5 var name = "abdurehman";
+// var combine = "hello " + name;
+// alert(combine)
+
+// alert exercise complete
+
+// Question N.02 Veriable for strings 
+
+// .1 var username = "abdurehman"
+// console.log(username)
+
+// .2 var messege = "welcome"
+// alert(messege)
