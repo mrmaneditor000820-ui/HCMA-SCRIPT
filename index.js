54993ebc375b3f99ab18991d60e8c637173a177c
dengerbhai00820-com <dengerbhai00820@gmail.com>
2025-12-10 16:53:10 -0800

    update

--- index.js
+++ index.js
@@ -24,3 +24,46 @@
 
 // .2 var messege = "welcome"
 // alert(messege)
+
+// .3 var name = "abdul" + "rehman";
+// console.log(name)
+
+// .4 var favoriteqoute = "hi i am abdurehman"
+// console.log(favoriteqoute)
+
+// .5 var city = "Welcome to [city]!"
+// alert(city)
+
+// Question N.03 .Variables for Numbers 
+
+// .1 var age = 18;
+// console.log(age)
+
+// .2 var age1 = 18;
+// var age2 = 18;
+// var result = age1 + age2
+// console.log(result)
+
+// .3 var number1 = 12;
+// var number2 = 4;
+// var result = number1 - number2
+// console.log(result)
+
+// .4 var number1 = 12;
+// var number2 = 4;
+// var result = number1 * number2
+// console.log(result)
+
+// .5 alert("You Are 20 Years Old.")
+
+// Question N.04 .Math Expressions:
+
+// .1 var value1 = x=10;
+// var value2 = y=5;
+// var result = value1 + value2
+// console.log(result)
+
+// .2 var yvalue = y=2;
+// var xvalue = x=4;
+// var result = y-x
+// alert(result)
